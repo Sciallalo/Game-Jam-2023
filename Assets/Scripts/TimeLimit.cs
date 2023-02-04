7a1057f366fbd6395134f83bef1acba0228544f1
Sciallalo <50951344+Sciallalo@users.noreply.github.com>
2023-02-04 13:09:12 +0100

    fix Scena1 + timeLimit script

---
+++ Assets/Scripts/TimeLimit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLimit : MonoBehaviour
+{
+    public float maxTime = 10f;
+    private float currentTime;
+    private bool runningTimer;
+    void Start()
+    {
+        currentTime = maxTime;
+        runningTimer = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        Debug.Log(currentTime);
+        if(currentTime <= 0)
+        {
+            runningTimer = false;
+            Debug.Log("Perso");
+        }
+    }
+}
