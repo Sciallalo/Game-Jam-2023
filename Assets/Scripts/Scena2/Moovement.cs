1dab64ebf0f07ac62fc5aacef6ff33bd8dcd2210
logicesecutor <58169924+logicesecutor@users.noreply.github.com>
2023-02-05 15:11:53 +0100

    .

--- Assets/Scripts/Scena2/Moovement.cs
+++ Assets/Scripts/Scena2/Moovement.cs
@@ -9,7 +9,7 @@ public class Moovement : MonoBehaviour
 {
     [SerializeField] GameObject other_hand;
 
-
+    [SerializeField] BlendCamera cameraBlend;
     [SerializeField] float range_z;
     [SerializeField] Transform center;
     [SerializeField] float approachingVelocity;
@@ -21,6 +21,7 @@ public class Moovement : MonoBehaviour
 
     bool WIN = false;
     float seed;
+    bool alreadySetted = false;
 
     Vector3 top_hand_offset;
 
@@ -49,28 +50,37 @@ public class Moovement : MonoBehaviour
 
         seed = UnityEngine.Random.Range(0, 100);
 
-        moovement_velocity = UnityEngine.Random.Range(3f, 5f);
-
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, UnityEngine.Random.Range(-range_z, range_z));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!WIN)
+        if (cameraBlend.cameraPositioned)
         {
-            float new_z_val = Mathf.Sin(seed) * range_z;
+            if (!WIN)
+            {
+                if (!alreadySetted)
+                {
+                    moovement_velocity = UnityEngine.Random.Range(3f, 5f);
 
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, new_z_val);
-            seed += moovement_velocity * Time.deltaTime;
-        }
+                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, UnityEngine.Random.Range(-range_z, range_z));
 
-        else
-        {
-            Vector3 new_top_position = Vector3.MoveTowards(gameObject.transform.GetChild(0).position, center.position, approachingVelocity * Time.deltaTime);
+                    alreadySetted = true;
+                }
+                float new_z_val = Mathf.Sin(seed) * range_z;
 
-            gameObject.transform.position = new_top_position - top_hand_offset;
-            
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, new_z_val);
+                seed += moovement_velocity * Time.deltaTime;
+            }
+
+            else
+            {
+                Vector3 new_top_position = Vector3.MoveTowards(gameObject.transform.GetChild(0).position, center.position, approachingVelocity * Time.deltaTime);
+
+                gameObject.transform.position = new_top_position - top_hand_offset;
+
+            }
         }
 
         if (Input.GetKey(KeyCode.RightControl))
